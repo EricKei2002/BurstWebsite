e2c977e87762ef46c206be224a66f7b21f7f5e11
EricKei2002 <yamamotokei587@gmail.com>
2023-07-18 12:13:20 +0900

    main

--- src/pages/homes/Vlog.js
+++ src/pages/homes/Vlog.js
@@ -7,24 +7,29 @@ const Vlog = () => {
       <h2 className="vlog-title">日々の日常をVlogにしました</h2>
       <div className="video-wrapper">
         <iframe
-          width="560"
-          height="315"
+            width="560"
+            height="315"
           src="https://www.youtube.com/embed/1Jzi065jvDs"
           title="YouTube Video 1"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
+        <p className="video-description">ここにビデオ1の説明文を書きます。</p>
       </div>
       <div className="video-wrapper">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/zw9oANUsiqw"
-          title="YouTube Video 2"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
+  <div className="video-description">
+    ここにビデオ2の説明文を書きます。
+  </div>
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/zw9oANUsiqw"
+    title="YouTube Video 2"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen
+  ></iframe>
+</div>
+
     </div>
   );
 };
