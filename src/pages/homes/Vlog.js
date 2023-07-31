6ffdebf2351e029208c01b901dbeb58278282a15
EricKei2002 <yamamotokei587@gmail.com>
2023-07-31 20:50:00 +0900

    headerは追加した

--- src/pages/homes/Vlog.js
+++ src/pages/homes/Vlog.js
@@ -16,20 +16,19 @@ const Vlog = () => {
         ></iframe>
         <p className="video-description">ここにビデオ1の説明文を書きます。</p>
       </div>
-      <div className="video-wrapper">
-  <div className="video-description">
-    ここにビデオ2の説明文を書きます。
-  </div>
-  <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/zw9oANUsiqw"
-    title="YouTube Video 2"
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-    allowFullScreen
-  ></iframe>
-</div>
-
+      <div className="video-wrapper" style={{marginTop: "50px"}}>
+        <div className="video-description">
+          ここにビデオ2の説明文を書きます。
+          </div>
+          <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/zw9oANUsiqw"
+          title="YouTube Video 2"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 };
